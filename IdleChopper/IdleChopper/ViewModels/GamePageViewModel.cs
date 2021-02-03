c42eb4ad00f0bed2e5acb07d4fedfc13b6d4f5cb
jorgen saeter <jorgen.sater@gmail.com>
2021-02-03 15:45:11 +1100

    Added working clicking for logs Added label to display your current log stash

---
+++ IdleChopper/IdleChopper/ViewModels/GamePageViewModel.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace IdleChopper.Views
+{
+    public partial class GamePageViewModel : ContentPage, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private double _Logs;
+        public double Logs
+        {
+            get => _Logs;
+            set
+            {
+                _Logs = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command LogClickCommand { get; }
+
+        public GamePageViewModel()
+        {
+            LogClickCommand = new Command(LogClickCommandClicked);
+        }
+
+        private void LogClickCommandClicked(object obj)
+        {
+            Logs += 1;
+            Console.WriteLine($"Clicked log. Logs: {Logs}");
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ IdleChopper/IdleChopper/Views/GamePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace IdleChopper.Views
+{
+    public partial class GamePage : ContentPage
+    {
+
+        public GamePage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new GamePageViewModel();
+        }
+    }
+}
