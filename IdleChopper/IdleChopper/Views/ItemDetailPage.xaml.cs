be027e8432650463a5a7b5e2cf3aef118aa56887
jorgen saeter <jorgen.sater@gmail.com>
2021-02-03 15:21:31 +1100

    Added project

---
+++ IdleChopper/IdleChopper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using IdleChopper.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace IdleChopper.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ IdleChopper/IdleChopper/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using IdleChopper.Models;
+using IdleChopper.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IdleChopper.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
